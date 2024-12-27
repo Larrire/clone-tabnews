d0eb424aefc5c173b58a52575e6589370eda260d
Larrire Lineker <47510492+Larrire@users.noreply.github.com>
2024-12-27 15:01:06 +0000

    adding the challenge URL

---
+++ pages/desafio/index.js
@@ -0,0 +1,17 @@
+function Desafio() {
+    const imgURL = "https://media.tenor.com/eXayvQU-Rh8AAAAe/gato-mago.png"
+
+    return (
+        <main>
+            <img width={300} src={imgURL}/>
+            <h3>
+                Estrelas que dançam, luas a brilhar,<br/>
+                2025 vem para encantar.<br/>
+                Sim salabim, com magia no ar,<br/>
+                o melhor ano está para começar!
+            </h3>
+        </main>
+    )
+}
+
+export default Desafio;
